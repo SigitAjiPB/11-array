82f29b8a812c22cbc7ad4aee08bec9e25d00d361
unknown <sigitnj89@gmail.com>
2024-01-17 21:56:33 +0700

    loop trough an array & accumulator pattern

--- 11-list-todo-project/11-todo-list.js
+++ 11-list-todo-project/11-todo-list.js
@@ -2,8 +2,10 @@ const todoList = [];
 
 function addTodo() {
   const inputElement = document.querySelector('.js-name-input');
-  const name = inputElement.ariaValueMax;
+  const name = inputElement.value;
 
   todoList.push(name);
   console.log(todoList);
+
+  inputElement.value = '';
 }
